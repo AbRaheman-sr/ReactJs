25a151ae30a1483fe9f5ca8f1249170d06b0741a
Abdul Raheman <abdulraheman0771@gmail.com>
2023-03-02 16:10:24 +0530

    Data Sharing with props. parent to child and child to parent

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,16 +1,33 @@
-import './App.css';
-import CounterProgram from './Components/CounterProgram';
-import ReactFirstProgram from './Components/ReactFirstProgram';
-
+import React, { useState } from "react";
+// import CounterProgram from './Components/CounterProgram';
+// import ReactFirstProgram from './Components/ReactFirstProgram';
+import Display from "./Components/Display";
+import Button from "./Components/Button";
 function App() {
-  
+  const [counter, setCounter] = useState(50);
+
+  function incrementHandeler() {
+    setCounter(counter + 5);
+  }
+  function decrementHandeler() {
+    setCounter(counter - 5)
+  }
   return (
-    <div className="App">
-      <ReactFirstProgram />
-      <CounterProgram counter={0} />
-      <CounterProgram counter={5} />
-      <CounterProgram counter={10} />
-    </div>
+    <>
+      <div className="App">
+        {/* this program for counter  */}
+        {/* <ReactFirstProgram />
+          <CounterProgram counter={0} />
+          <CounterProgram counter={5} />
+          <CounterProgram counter={10} /> */}
+      </div>
+
+      <div>
+        <Display messege={counter} />
+        <Button onClickHandeler={incrementHandeler}/>
+        <Button onClickHandeler={decrementHandeler}/>
+      </div>
+    </>
   );
 }
 
